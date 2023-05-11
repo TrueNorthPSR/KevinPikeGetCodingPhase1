2c2de2b8d1c584e7d766a4bf53c661efffc9f17f
Kevin Pike <kevin.pike@truenorthpsr.com>
2023-05-11 20:37:32 -0230

    11 May commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ submitBtn.addEventListener("click", function() {
 const selectedAnswer = document.querySelector('input[name="q1"]:checked');
 
 if (selectedAnswer) {
-const confirmMsg = You are about to submit ${selectedAnswer.value};
+const confirmMsg = `You are about to submit ${selectedAnswer.value}`;
 alert(confirmMsg);
 } else {
 alert("You cannot submit an answer until you first select an answer.");
